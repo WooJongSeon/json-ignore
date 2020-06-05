152efb230984af0b8813bbc95a00d1970869e824
WooJongSeon <jswoo030@gmail.com>
2020-06-05 17:22:17 +0900

    ts 로 변환

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@
  * @param {없앨 필드 목록을 적습니다.} fields ['필드', '필드']
  * @param {객체를 보냅니다.} obj
  */
-const jsonIgnore = (fields, obj) => {
+export const jsonIgnore = (fields: Array<any>, obj: any) => {
   fields.map((key) => {
     delete obj[key]
   })
   return obj;
 }
 
-
-module.exports = jsonIgnore;
